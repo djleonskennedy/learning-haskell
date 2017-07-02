943c503db550870f1fb8af75ca682b0ded4773d6
Yuriy Yakovenko <yuriy.yakovenko@infopulse.com>
2017-07-02 20:13:32 +0300

    Lists finished

--- src/basics/main.hs
+++ src/basics/main.hs
@@ -1,13 +1,22 @@
 --- simple list defenition in haskel
-
+-- List a
 data List a = Nil | Const a (List a) deriving Show
+-- similar to:
+-- data [] a = [] | a : ([] a)
+-- or:
+-- data [] a = [] | a : [a]
 
 len                 ::  List a -> Int
 len Nil             =   0
 len (Const _ xs)    =   1 + len xs
 
-myList = Const 1 (Const 2 (Const 3 Nil))
+len'                ::  [a] -> Int
+len' []             =   0
+len' (x: xs)    =   1 + len' xs
 
 head'            ::  [a] -> a
 head'  (x:_)     =   x
 head'  []        =   error "head error: empty list"
+
+myList = Const 1 (Const 2 (Const 3 Nil))
+myAnotherList = 'a': ('b': ('c': []))
