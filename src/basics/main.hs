33fe63206b5166d5b56f83a5a181b3cc74dadb7e
Yuriy Yakovenko <yuriy.yakovenko@infopulse.com>
2017-07-02 19:55:56 +0300

    setup repos pass

--- src/basics/main.hs
+++ src/basics/main.hs
@@ -1,3 +1,5 @@
+--- simple list defenition in haskel
+
 data List a = Nil | Const a (List a) deriving Show
 
 len                 ::  List a -> Int
@@ -5,3 +7,7 @@ len Nil             =   0
 len (Const _ xs)    =   1 + len xs
 
 myList = Const 1 (Const 2 (Const 3 Nil))
+
+head'            ::  [a] -> a
+head'  (x:_)     =   x
+head'  []        =   error "head error: empty list"
